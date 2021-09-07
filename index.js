409e10fdeb09d0aef1c39dfdf43d15a7486a2a3a
Raven <ravenkoehler@gmail.com>
2021-09-07 12:06:26 -0400

    commit tree a2663b3b43c18ddee160b2e84d8bf8748656fc77

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData() {
+    return fetch("http://localhost:3000/users", configObj)
+    .then(resp => resp.json())
+    .then(function(obj) {
+        const id = obj.id;
+        const p = document.createElement("p");
+        p.innerHTML = id;
+        document.querySelector("body").appendChild(p);
+    })
+    .catch(function(error) {
+        const errorDiv = document.createElement("div");
+        errorDiv.innerHTML = error.message
+        document.querySelector("body").appendChild(errorDiv);
+    })
+}
+
+let dataObj = {
+    name: "Steve",
+    email: "steve@steve.com"
+};
+
+let configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(dataObj)
+};
